c5f40b084df928fe43fc62c8ab956302cd3f5656
Angelo Santos <gabrielange883@gmail.com>
2024-07-09 14:13:48 -0300

    fixing Img bug #4

--- angelo/src/components/About.tsx
+++ angelo/src/components/About.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 const About = () => (
-    <section className="min-h-screen min-h-3 my-12 px-4 sm:px-6 lg:px-8 flex flex-col lg:flex-row items-center lg:items-start text-center lg:text-left">
+    <section className="min-h-1.5 my-12 px-4 sm:px-6 lg:px-8 flex flex-col lg:flex-row items-center lg:items-start text-center lg:text-left">
         <div className="lg:flex-2">
             <h2 className="text-3xl font-bold text-white sm:text-2xl lg:text-3xl">About</h2>
             <p className="text-lg text-white mt-4 mx-auto max-w-2xl sm:text-xl lg:text-2xl">
@@ -13,8 +13,8 @@ const About = () => (
             </p>
         </div>
         <div className="mt-6 lg:mt-0 lg:ml-6">
-            <Image src="/Angeloprofile.png" alt="Angelo"
-                 className="rounded-full w-32 h-32 sm:w-40 sm:h-40 lg:w-48 lg:h-48 object-cover"/>
+            <Image src="/Angeloprofile.png" alt="Angelo" width={200} height={200}
+                   className="rounded-full w-32 h-32 sm:w-40 sm:h-40 lg:w-48 lg:h-48 object-cover"/>
         </div>
     </section>
 );
--- angelo/src/components/Contact.tsx
+++ angelo/src/components/Contact.tsx
@@ -9,9 +9,15 @@ const Contact = () => (
         <div className="mt-auto">
             <a href="mailto:Gabrielange883@gmail.com" className="text-lg mt-8 text-purple-700 font-bold">Gabrielange883@gmail.com</a>
             <div className="flex justify-center mt-6 space-x-4">
-                <a href="#" className="text-white"><Image src="iconesSocialMedias/linkedin.svg" alt="LinkedIn" className="w-6 h-6" /></a>
-                <a href="#" className="text-white"><Image src="iconesSocialMedias/github.svg" alt="GitHub" className="w-6 h-6" /></a>
-                <a href="#" className="text-white"><Image src="iconesSocialMedias/gmail.svg" alt="Gmail" className="w-6 h-6" /></a>
+                <a href="#" className="text-white">
+                    <Image src="/iconesSocialMedias/linkedin.svg" alt="LinkedIn" width={24} height={24} />
+                </a>
+                <a href="#" className="text-white">
+                    <Image src="/iconesSocialMedias/github.svg" alt="GitHub" width={24} height={24} />
+                </a>
+                <a href="#" className="text-white">
+                    <Image src="/iconesSocialMedias/gmail.svg" alt="Gmail" width={24} height={24} />
+                </a>
             </div>
         </div>
     </section>
