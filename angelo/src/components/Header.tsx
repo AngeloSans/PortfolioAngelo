f790bfee4672160a0bdbc458ae8b597ea71bad44
Angelo Santos <gabrielange883@gmail.com>
2024-08-01 11:25:43 -0300

    button color

--- angelo/src/components/Header.tsx
+++ angelo/src/components/Header.tsx
@@ -4,14 +4,14 @@ import { useEffect, useState } from 'react';
 
 const Header = () => {
     const [text, setText] = useState('');
-    const targetText = 'Sooftware Engineer';
-    const typingSpeed = 100; 
+    const [isClicked, setIsClicked] = useState(false);
+    const targetText = 'Sooftware Engineer'; 
+    const typingSpeed = 100;
 
     useEffect(() => {
         let index = 0;
         const interval = setInterval(() => {
             setText((prev) => {
-                
                 if (index < targetText.length) {
                     return prev + targetText[index];
                 }
@@ -26,6 +26,10 @@ const Header = () => {
         return () => clearInterval(interval);
     }, []);
 
+    const handleClick = () => {
+        setIsClicked(true);
+    };
+
     return (
         <header className="bg-custom-dark text-center my-1 px-4 sm:px-6 lg:px-8 min-h-screen h-[70vh]">
             <div className="flex justify-center">
@@ -39,10 +43,12 @@ const Header = () => {
             <p className="text-lg text-white mt-2 sm:mt-4 lg:mt-6 sm:text-xl lg:text-2xl max-w-2xl mx-auto">
                 I&apos;m a software engineer from Brazil, who loves to develop new solutions.
             </p>
-            <a href="/AngeloSantosCurriculo.pdf"
-               className="inline-block mt-6 px-6 py-3 text-white rounded-lg shadow-lg hover:bg-purple-700 sm:mt-8 lg:mt-10"
-               style={{ background: '#5007C8', borderRadius: '50px' }}
-               download
+            <a
+                href="/AngeloSantosCurriculo.pdf"
+                className={`inline-block mt-6 px-6 py-3 font-bolder rounded-lg shadow-lg sm:mt-8 lg:mt-10 ${isClicked ? 'bg-white text-black' : 'bg-[#5007C8] text-white'} hover:bg-white hover:text-black hover:border-purple`}
+                style={{ borderRadius: '50px', textDecoration: 'none' }}
+                onClick={handleClick}
+                download
             >
                 Download CV
             </a>
