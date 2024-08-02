a1895c4a63ae72a1fbe5bf4fd210108c3151729c
Angelo Santos <gabrielange883@gmail.com>
2024-08-02 12:33:16 -0300

    contacts

--- angelo/src/components/Contact.tsx
+++ angelo/src/components/Contact.tsx
@@ -9,14 +9,14 @@ const Contact = () => (
         <div className="mt-auto mb-8">
             <a href="mailto:Gabrielange883@gmail.com" className="text-lg mt-8 text-purple-700 font-bold">Gabrielange883@gmail.com</a>
             <div className="flex justify-center mt-6 space-x-4">
-                <a href="https://www.linkedin.com" className="text-white">
-                    <Image src="/IconesSocialMedias/linkedin.svg" alt="LinkedIn" width={24} height={24} />
+                <a href="https://www.linkedin.com" className="text-white transform transition-transform hover:translate-y-[-5px]">
+                    <Image src="/IconesSocialMedias/linkedin.svg" alt="LinkedIn" width={30} height={30} />
                 </a>
-                <a href="https://github.com" className="text-white">
-                    <Image src="/IconesSocialMedias/github.svg" alt="GitHub" width={24} height={24} />
+                <a href="https://github.com" className="text-white transform transition-transform hover:translate-y-[-5px]">
+                    <Image src="/IconesSocialMedias/github.svg" alt="GitHub" width={30} height={30} />
                 </a>
-                <a href="https://mail.google.com" className="text-white">
-                    <Image src="/IconesSocialMedias/gmail.svg" alt="Gmail" width={24} height={24} />
+                <a href="https://mail.google.com" className="text-white transform transition-transform hover:translate-y-[-5px]">
+                    <Image src="/IconesSocialMedias/gmail.svg" alt="Gmail" width={30} height={30} />
                 </a>
             </div>
         </div>
