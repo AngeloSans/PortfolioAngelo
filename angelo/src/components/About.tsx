e089a73f4cbecbef8765088d78a1d563a81d7880
Angelo Santos <gabrielange883@gmail.com>
2024-08-01 21:00:24 -0300

    about

--- angelo/src/components/About.tsx
+++ angelo/src/components/About.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 const About = () => (
     <section className="bg-custom-dark min-h-1.5 my-12 px-4 sm:px-6 lg:px-8 flex flex-col lg:flex-row items-center lg:items-start text-center lg:text-left">
         <div className="lg:flex-2">
-            <h2 className="text-3xl font-bold text-white sm:text-2xl lg:text-3xl">About</h2>
-            <p className="text-lg text-white mt-4 mx-auto max-w-2xl sm:text-xl lg:text-2xl">
+            <h2 className="text-3xl text-center font-bold text-white sm:text-2xl lg:text-3xl">About</h2>
+            <p className="text-lg text-center text-white mt-4 mx-auto max-w-2xl sm:text-xl lg:text-2xl">
                 Hi, my name is Angelo, I&apos;m based in Brazil.<br/>
                 I&apos;ve been learning software development since 2022.<br/>
                 I&apos;ve been developing some skills and earning some recognition, for example,<br/>
@@ -14,7 +14,7 @@ const About = () => (
         </div>
         <div className="mt-6 lg:mt-0 lg:ml-6">
             <Image src="/Angeloprofile.png" alt="Angelo" width={200} height={200}
-                   className="rounded-full w-32 h-32 sm:w-40 sm:h-40 lg:w-48 lg:h-48 object-cover"/>
+                   className="rounded-full mt-10 w-32 h-32 sm:w-40 sm:h-40 lg:w-48 lg:h-48 object-cover"/>
         </div>
     </section>
 );
